133e8d190d920dcefa64d96d4dcca3fdcc5398f3
Michael David Pascual <mdnpascual@yahoo.com.ph>
2017-08-30 19:05:18 -0600

    steam folder backup commands

--- winUsefulScripts.bat
+++ winUsefulScripts.bat
@@ -3,3 +3,10 @@ SetLocal EnableDelayedExpansion
 
 //Pipe twitch steams/VODs to VLC
 streamlink --player="D:\Program Files (x86)\VideoLAN\VLC\vlc.exe" --player-passthrough hls https://www.twitch.tv/dota2ti best
+
+//Steam folder backup
+//Copy new files only
+robocopy <sourcepath> <destination path> /E /XO /XN
+
+//Copy new and updated files
+robocopy <sourcepath> <destination path> /E /XO
