136cbd4ebd8527abee2b2af3e998f2205576d682
Michael David Pascual <mdnpascual@yahoo.com.ph>
2018-09-11 14:59:28 -0600

    Update linuxUsefulScripts.sh

--- linuxUsefulScripts.sh
+++ linuxUsefulScripts.sh
@@ -3,3 +3,6 @@ rm $(ls -lrt .|grep " Aug 30 10:37"|awk '{print $9}')
 
 // Executing command line inside awk to do calculations
 awk 'BEGIN{ "ls -la /home/helcim/images/public/8/472/ | wc -l " | getline publicCount; publicCount = publicCount - 3 ; "ls -la /home/helcim/images/public/8/472/*svg | wc -l" | getline svgPublicCount; totalPublic = ((publicCount - svgPublicCount)/3) + svgPublicCount; "ls -la /home/helcim/images/private/8/472/ | wc -l" | getline privateCount; privateCount = privateCount - 3; "ls -la /home/helcim/images/private/8/472/*svg | wc -l" | getline svgPrivateCount; totalPrivate = ((privateCount - svgPrivateCount)/3) + svgPrivateCount; print totalPublic + totalPrivate}'
+
+// DEBUG  
+tail -f -n 30 /var/log/httpd/error_log
